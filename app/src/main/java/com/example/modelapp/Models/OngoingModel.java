fc26dddd6ce4f6d27aeb2f34dca1d728e37516b1
harshit <harshit.tiwari2020@vitbhopal.ac.in>
2022-10-05 22:08:02 +0530

    ongoing model and adapter

--- app/src/main/java/com/example/modelapp/Models/OngoingModel.java
+++
@@ -1,5 +0,0 @@
-package com.example.modelapp.Models;
-
-public class OngoingModel {
-
-}
--- app/src/main/java/com/example/modelapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/modelapp/ui/home/HomeFragment.java
@@ -1,23 +1,19 @@
 package com.example.modelapp.ui.home;
 
-import android.hardware.lights.LightState;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.modelapp.Adapters.UpcommingAdapter;
 import com.example.modelapp.Models.UpcommingModel;
 import com.example.modelapp.R;
-import com.example.modelapp.databinding.FragmentHomeBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -29,7 +25,7 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView up_rec;
+    RecyclerView up_rec, on_rec;
     FirebaseFirestore db;
 
     public  HomeFragment() {
@@ -77,7 +73,11 @@ public class HomeFragment extends Fragment {
 
 
 
-        // ongoing rec
+
+
+
+
+
 
 
 
