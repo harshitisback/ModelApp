092b1b1d2489679c15984d8d36e846d7e3158b07
harshit <harshit.tiwari2020@vitbhopal.ac.in>
2022-10-06 09:07:59 +0530

    ongoing model and adapter

--- app/src/main/java/com/example/modelapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/modelapp/ui/home/HomeFragment.java
@@ -11,7 +11,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.modelapp.Adapters.OngoingAdapter;
 import com.example.modelapp.Adapters.UpcommingAdapter;
+import com.example.modelapp.Models.OngoingModel;
 import com.example.modelapp.Models.UpcommingModel;
 import com.example.modelapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,7 +27,8 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView up_rec, on_rec;
+    RecyclerView up_rec;
+    RecyclerView on_rec;
     FirebaseFirestore db;
 
     public  HomeFragment() {
@@ -38,6 +41,10 @@ public class HomeFragment extends Fragment {
     List<UpcommingModel> upcommingModelList;
     UpcommingAdapter upcommingAdapter;
 
+    // ongoing events
+    List<OngoingModel> ongoingModelList;
+    OngoingAdapter ongoingAdapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -46,6 +53,7 @@ public class HomeFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
 
         up_rec = root.findViewById(R.id.upc_rec);
+        on_rec = root.findViewById(R.id.ongoing_rec);
 
         // upcomiing evets set
         up_rec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
@@ -67,6 +75,30 @@ public class HomeFragment extends Fragment {
                             }
                         } else {
                             Toast.makeText(getActivity(),"Error",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+// ongoing event set
+        on_rec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL, false));
+        ongoingModelList = new ArrayList<>();
+        ongoingAdapter = new OngoingAdapter(getActivity(),ongoingModelList);
+        on_rec.setAdapter(ongoingAdapter);
+
+
+        db.collection("OngoingEvents")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                OngoingModel ongoingModel = document.toObject(OngoingModel.class);
+                                ongoingModelList.add(ongoingModel);
+                                ongoingAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
